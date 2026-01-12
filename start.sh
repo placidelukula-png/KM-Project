8b8834c7cb4abe706c705a135ff7b8c151bc350d
Placide <placide.lukula@gmail.com>
2026-01-11 20:52:54 -0600

    Activate venv in start.sh for Render

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-python3 -m gunicorn app_flask_postgres:app --bind 0.0.0.0:$PORT
+# Render place souvent le venv ici :
+if [ -f "/opt/render/project/src/.venv/bin/activate" ]; then
+  source /opt/render/project/src/.venv/bin/activate
+elif [ -f ".venv/bin/activate" ]; then
+  source .venv/bin/activate
+fi
+
+# Lancer gunicorn depuis l'environnement (venv)
+exec gunicorn app_flask_postgres:app --bind 0.0.0.0:$PORT
