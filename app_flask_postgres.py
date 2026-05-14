c55f89c38f32af8d99a5da1012d8d8f3f59ec8d2
placidelukula-png <placide.lukula@gmail.com>
2026-05-14 12:26:29 -0500

    Nkambu

--- app_flask_postgres.py
+++ app_flask_postgres.py
@@ -2914,7 +2914,7 @@ def import_mouvements():
 ###
     difference = (datetime.now(timezone.utc) - session["idempotency_time"]).total_seconds() if "idempotency_time" in session else None
     session["idempotency_time"] = datetime.now(timezone.utc)
-    if difference is not None and difference < 5:  # seuil de 5 secondes pour éviter les doubles clics rapides
+    if difference is not None and difference < 15:  # seuil de 15 secondes pour éviter les doubles clics rapides
        flash("Transfert bloqué en raison d'une tentative de double clic rapide", "danger")
        return render_template_string(IMPORT_PAGE, message="Transfert bloqué : Veuillez attendre quelques secondes avant de réessayer.", is_error=True, stats="")
 ###    
@@ -3650,7 +3650,8 @@ def download_csv():
             with cur.copy("""
                 COPY (
 /*                    SELECT * FROM mouvements WHERE regie IS NOT NULL*/
-                    SELECT phone, firstname, lastname, adresse FROM membres WHERE currentstatute IN ('probatoire', 'actif')
+/*                    SELECT phone, firstname, lastname, adresse FROM membres WHERE currentstatute IN ('probatoire', 'actif') */
+                      SELECT phone, firstname, lastname, adresse FROM membres WHERE mentor = "818329793" OR phone = "818329793" 
                 ) TO STDOUT WITH CSV HEADER
             """) as copy:
                 for data in copy:
