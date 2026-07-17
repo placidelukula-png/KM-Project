3771b0e229deab13100aabd658666a87eee2fee8
placidelukula-png <placide.lukula@gmail.com>
2026-07-17 12:29:02 -0500

    invitation à l'inscription et 'à propos'

--- app_flask_postgres.py
+++ app_flask_postgres.py
@@ -1879,12 +1879,12 @@ LOGIN_PAGE = """
         <div class="navigation-buttons" style="margin-top: 5px; display: flex; gap: 10px;border-radius: 20px;justify-content: center; align-items: center;">
             <!-- Bouton Inscription -->
             <a href="{{ url_for('add_member_route') }}">
-                <button type="button" class="btn btn-primary" style="font-size: 0.7em;;color: #000001;background-color: #100000">Veuillez vous inscrire librement</button>
+                <button type="button" class="btn btn-primary" style="font-size: 0.7em;color: #32CD32">Veuillez vous inscrire librement</button>
             </a>
 
             <!-- Bouton Infos Association rgba(25,25,112,1) or rgba(0,0,128,1)-->
             <a href="{{ url_for('infos_association') }}">
-                <button type="button" class="btn btn-primary" style="font-size: 0.7em;;color: #32CD32;background-color: #008000">A propos de notre association</button>
+                <button type="button" class="btn btn-primary" style="font-size: 0.7em;color: #32CD32">A propos de notre association</button>
             </a>
         </div>
         
